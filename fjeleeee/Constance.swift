2b80dd8f3278ec99d380838d2780821412a22fd2
fjelde10 <brianjfjelde@hotmail.com>
2015-12-10 11:52:37 +0100

    Layout

---
+++ fjeleeee/Constance.swift
@@ -0,0 +1,11 @@
+//
+//  Constance.swift
+//  fjeleeee
+//
+//  Created by brian on 10/12/15.
+//  Copyright © 2015 bjfmedia. All rights reserved.
+//
+import UIKit
+import Foundation
+
+let SHADOW_COLOR: CGFloat = 157.00 / 255.0
